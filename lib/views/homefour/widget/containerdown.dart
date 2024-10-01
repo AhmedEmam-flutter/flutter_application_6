cb5e5b4be15a8fdc4c0034c3d309bee60e03b2a1
AhmedEmam-flutter <aemam4083@gmail.com>
2024-10-01 12:13:29 -0700

    commit

--- lib/views/homefour/widget/containerdown.dart
+++ lib/views/homefour/widget/containerdown.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Containerdown extends StatelessWidget {
+  final String image;
+  final String text1;
+  final String price;
+  final String place;
+
   Containerdown({
     super.key,
     required this.image,
@@ -8,10 +13,6 @@ class Containerdown extends StatelessWidget {
     required this.price,
     required this.place,
   });
-  String image;
-  String text1;
-  String price;
-  String place;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +28,7 @@ class Containerdown extends StatelessWidget {
             color: Colors.grey.withOpacity(0.1),
             spreadRadius: 5,
             blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
+            offset: Offset(0, 3),
           ),
         ],
       ),
@@ -35,53 +36,45 @@ class Containerdown extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Row(
           children: [
-            Image.asset(image, width: 100, height: 100, fit: BoxFit.fill),
-            SizedBox(
-              width: 20,
+            Flexible(
+              child: Image.asset(image, fit: BoxFit.cover),
+              flex: 3,
             ),
+            SizedBox(width: 20),
             Expanded(
+              flex: 7,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(text1,style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue[900],fontSize: 20),),
-                      Spacer(),
-                      Text(price,style: TextStyle(color: Colors.blue[900]),),
+                      Text(
+                        text1,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[900],
+                            fontSize: MediaQuery.of(context).size.width * 0.05),
+                      ),
+                      Text(
+                        price,
+                        style: TextStyle(color: Colors.blue[900]),
+                      ),
                     ],
                   ),
-                  Row(
-                    children: [
-                      Text(place),
-                    ],
-                  ),
+                  SizedBox(height: 10),
+                  Text(place),
+                  SizedBox(height: 10),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text('2 ', style: TextStyle(color: Colors.green)),
+                      Text('2', style: TextStyle(color: Colors.green)),
                       Icon(Icons.bed, color: Colors.green),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.19,
-                      ),
-                      Text(
-                        '2 ',
-                        style: TextStyle(color: Colors.green),
-                      ),
-                      Icon(
-                        Icons.bathtub,
-                        color: Colors.green,
-                      ),
-                      Spacer(),
-                      Text(
-                        '2 ',
-                        style: TextStyle(color: Colors.green),
-                      ),
-                      Icon(
-                        Icons.car_rental,
-                        color: Colors.green,
-                      ),
+                      Text('2', style: TextStyle(color: Colors.green)),
+                      Icon(Icons.bathtub, color: Colors.green),
+                      Text('2', style: TextStyle(color: Colors.green)),
+                      Icon(Icons.car_rental, color: Colors.green),
                     ],
                   )
                 ],
