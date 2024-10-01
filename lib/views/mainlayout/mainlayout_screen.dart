628f292a7ef589819b4c1f0a967241d1160a7f11
AhmedEmam-flutter <aemam4083@gmail.com>
2024-10-01 16:27:07 -0700

    Update mainlayout_screen.dart

--- lib/views/mainlayout/mainlayout_screen.dart
+++ lib/views/mainlayout/mainlayout_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_6/views/chat/chat_screen.dart';
 import 'package:flutter_application_6/views/fliterpropertry/fliter_screem.dart';
 import 'package:flutter_application_6/views/homefour/home_four.dart';
 import 'package:flutter_application_6/views/login/login_screen.dart';
+import 'package:flutter_application_6/views/matchedpropertery/matched_screen.dart';
 import 'package:flutter_application_6/views/register/register_screen.dart';
 import 'package:flutter_application_6/views/setting/setting_screen.dart';
 import 'package:flutter_application_6/views/splash/splash_screen.dart';
@@ -16,49 +17,53 @@ class MainlayoutScreen extends StatefulWidget {
 
 class _TraninngState extends State<MainlayoutScreen> {
   int c = 0;
- 
+
   @override
- 
   List<Widget> screen = [
     HomeFour(),
-    FliterScreem(),
+    MatchedScreen(),
     RegisterScreen(),
-   ChatScreen(),
+    ChatScreen(),
     SettingScreen(),
   ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: screen[c],
       bottomNavigationBar: BottomNavigationBar(
-        
-        backgroundColor: Colors.white,
+          backgroundColor: Colors.white,
           type: BottomNavigationBarType.fixed,
           currentIndex: c,
           showUnselectedLabels: false,
-           showSelectedLabels:false,
+          showSelectedLabels: false,
           onTap: (index) {
             setState(() {
               c = index;
             });
           },
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-            
-            BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'serach'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: 'home'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark), label: 'serach'),
             BottomNavigationBarItem(
                 icon: Column(
                   children: [
-                    CircleAvatar( 
-                      radius: 30,
-                      backgroundColor: Colors.purple[300],
-                      child: Icon(Icons.apps)),
-                      SizedBox(height: 12,
-                      width: MediaQuery.of(context).size.width*0.001,),
+                    CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.purple[300],
+                        child: const Icon(Icons.apps)),
+                    SizedBox(
+                      height: 12,
+                      width: MediaQuery.of(context).size.width * 0.001,
+                    ),
                   ],
-                ), label: 'home page'),
-            BottomNavigationBarItem(
+                ),
+                label: 'home page'),
+            const BottomNavigationBarItem(
                 icon: Icon(Icons.chat), label: 'chat'),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings'),
           ]),
     );
   }
