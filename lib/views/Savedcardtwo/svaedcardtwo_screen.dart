7445d7c56bdbab62d64d815495f5e6f2cf3e3283
AhmedEmam-flutter <aemam4083@gmail.com>
2024-10-01 16:21:43 -0700

    Update svaedcardtwo_screen.dart

--- lib/views/Savedcardtwo/svaedcardtwo_screen.dart
+++ lib/views/Savedcardtwo/svaedcardtwo_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_6/views/Savedcardtwo/list/listtitlesved.dart';
 import 'package:flutter_application_6/views/savedcards/widget/cardswipe.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
-class SvaedcardtwoScreen extends StatelessWidget {
-   SvaedcardtwoScreen({super.key});
-   
-  
+class SvaedcardtwoScreen extends StatefulWidget {
+  const SvaedcardtwoScreen({super.key});
+
+  @override
+  _SvaedcardtwoScreenState createState() => _SvaedcardtwoScreenState();
+}
+
+class _SvaedcardtwoScreenState extends State<SvaedcardtwoScreen> {
+  int ishow = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +19,7 @@ class SvaedcardtwoScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(
+        title: const Text(
           'Cards',
           style: TextStyle(color: Colors.black),
         ),
@@ -22,7 +28,7 @@ class SvaedcardtwoScreen extends StatelessWidget {
           onTap: () {
             Navigator.pop(context);
           },
-          child: Icon(
+          child: const Icon(
             Icons.arrow_back_ios_new_outlined,
             color: Colors.black,
           ),
@@ -36,87 +42,117 @@ class SvaedcardtwoScreen extends StatelessWidget {
               height: MediaQuery.of(context).size.height * 0.4,
               child: Cardswipe(),
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.1,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(20),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ToggleSwitch(
+                    minWidth: MediaQuery.of(context).size.width * 0.8,
+                    minHeight: MediaQuery.of(context).size.height * 0.08,
+                    initialLabelIndex: ishow,
+                    activeBgColor: [Colors.red],
+                    inactiveBgColor: Colors.grey,
+                    labels: ['Incoming', 'Outgoing'],
+                    onToggle: (index) {
+                      setState(() {
+                        ishow = index!;
+                      });
+                    },
+                  ),
+                ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      height: 200,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Expanses',
-                          style: TextStyle(color: Colors.white),
+            ),
+            ishow == 0
+                ? Column(
+                    children: [
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.home,
+                          color: Colors.red,
+                          size: 30,
                         ),
-                        style:
-                            ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                        title: 'Pay Builders',
+                        subtitle: 'Jan 20, 2019',
+                        price: '\$2000',
                       ),
-                    ),
-                    TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Incomeing',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ))
-                  ],
-                ),
-              ),
-            ),
-            Listtitlesved(
-              icn: Icon(
-                Icons.home,
-                color: Colors.red,
-                size: 30,
-              ),
-              title: 'pay Builders',
-              subtitle: 'Jan 20,2019',
-              price: '\$2000',
-            ),
-            Listtitlesved(
-              icn: Icon(
-                Icons.home,
-                color: Colors.blue,
-                size: 30,
-              ),
-              title: 'Rent House',
-              subtitle: 'Jan 7,2022',
-              price: '\$4000',
-            ),
-            Listtitlesved(
-              icn: Icon(
-                Icons.person,
-                color: Colors.orange,
-                size: 30,
-              ),
-              title: 'BRoker',
-              subtitle: 'Jan 20,2024',
-              price: '\$7000',
-            ),
-            Listtitlesved(
-              icn: Icon(
-                Icons.home_filled,
-                color: Colors.green,
-                size: 30,
-              ),
-              title: 'Buy House',
-              subtitle: 'Jan 1,2020',
-              price: '\$9000',
-            ),
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.home,
+                          color: Colors.blue,
+                          size: 30,
+                        ),
+                        title: 'Rent House',
+                        subtitle: 'Jan 7, 2022',
+                        price: '\$4000',
+                      ),
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.person,
+                          color: Colors.orange,
+                          size: 30,
+                        ),
+                        title: 'Broker',
+                        subtitle: 'Jan 20, 2024',
+                        price: '\$7000',
+                      ),
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.home_filled,
+                          color: Colors.green,
+                          size: 30,
+                        ),
+                        title: 'Buy House',
+                        subtitle: 'Jan 1, 2020',
+                        price: '\$9000',
+                      ),
+                    ],
+                  )
+                : Column(
+                    children: [
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.person,
+                          color: Colors.orange,
+                          size: 30,
+                        ),
+                        title: 'Broker',
+                        subtitle: 'Jan 20, 2024',
+                        price: '\$7000',
+                      ),
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.home_filled,
+                          color: Colors.green,
+                          size: 30,
+                        ),
+                        title: 'Buy House',
+                        subtitle: 'Jan 1, 2020',
+                        price: '\$9000',
+                      ),
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.home,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                        title: 'Pay Builders',
+                        subtitle: 'Jan 20, 2019',
+                        price: '\$2000',
+                      ),
+                      Listtitlesved(
+                        icn: const Icon(
+                          Icons.home,
+                          color: Colors.blue,
+                          size: 30,
+                        ),
+                        title: 'Rent House',
+                        subtitle: 'Jan 7, 2022',
+                        price: '\$4000',
+                      ),
+                    ],
+                  ),
           ],
         ),
       ),
